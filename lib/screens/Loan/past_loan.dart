c6e564872c3e7f53632eaf8388800bff4bd24da5
Manuel-Seyram <51480103+Manuel-Seyram@users.noreply.github.com>
2022-12-16 09:31:07 +0000

    - Refactor

--- lib/screens/Loan/past_loan.dart
+++ lib/screens/Loan/past_loan.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:instacash/screens/Loan/delivery_method.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class   PastLoan extends StatelessWidget {
@@ -111,6 +113,79 @@ class   PastLoan extends StatelessWidget {
               )
             ],
           ),
+        ),
+        const SizedBox(height: 180,),
+        SizedBox(
+          height: 90,
+          width: 350,
+          child: FloatingActionButton.large(
+            onPressed: (){
+              PersistentNavBarNavigator.pushNewScreen(
+                              context,
+                              screen: const DeliveryMethod(),
+                              withNavBar: false,
+                            );
+            },
+            backgroundColor: Colors.blue,
+            clipBehavior: Clip.antiAlias,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            ),
+            hoverElevation: 3.0,
+            child: Row(
+              children: [
+                   const SizedBox(width: 5.0,),
+                    Image.asset('assets/GetLoan.png', height: 60, width: 60),
+                    Column(
+                      children: [
+                        const SizedBox(height: 20.0,),
+                        Text(
+                          ' Your next Loan',
+                          style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        const SizedBox(height: 5.0,),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 50.0),
+                          child: Text(
+                            'GHS 800',
+                            style: GoogleFonts.poppins(
+                              textStyle: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 30.0,),
+                    Container(
+                      height: 40,
+                      width: 90,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Get loan',
+                          style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                      ),
+                    ),
+              ],
+            ),
+            ),
         )
       ],
     );
