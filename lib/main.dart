6ba4aa61125c2c412a24bd51123ddbd6f4e5349e
Manuel-Seyram <51480103+Manuel-Seyram@users.noreply.github.com>
2023-02-20 12:08:11 +0000

    dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 // ignore: depend_on_referenced_packages
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:instacash/screens/Customer%20Onboarding/sign_up/bank_information.dart';
@@ -34,47 +35,49 @@ import '../screens/Customer Onboarding/splash screen/splash_screen.dart';
 
 void main() {
   initializeDateFormatting().then((_)=>
-   runApp(MaterialApp(
-    theme: ThemeData(
-      colorScheme: const ColorScheme.light(
-        primary:Color.fromARGB(249, 40, 68, 194)
+   runApp(ProviderScope(
+     child: MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary:Color.fromARGB(249, 40, 68, 194)
+        ),
+        primaryColor: const Color.fromARGB(249, 40, 68, 194),
+        focusColor: const Color.fromARGB(249, 40, 68, 194)
       ),
-      primaryColor: const Color.fromARGB(249, 40, 68, 194),
-      focusColor: const Color.fromARGB(249, 40, 68, 194)
-    ),
-    debugShowCheckedModeBanner: false,
-    initialRoute: '/',
-      routes: {
-        '/': ((context) => const SplashScreen()),
-        '/onboarding':(context) => const Landing(),
-        '/phonefield':(context) => const PhoneField(),
-        '/otp':(context) => const Otp(),
-        '/basicinformation':(context) => const BasicInformation(),
-        '/login':(context) => const Login(),
-        '/idproof':(context) => const Proof(),
-        '/verifyfront':(context) => const Frontcard2(),
-        '/selfie':(context) => const Selfie(),
-        '/selfieverify':(context) => const Selfieverify(),
-        '/forgotpassword1':(context) => const Forgotpassword1(),
-        '/forgotpassword2':(context) => const Forgotpassword2(),
-        '/forgotpassword3':(context) => const Forgotpassword3(),
-        '/bankinformation':(context) => const  BankInformation(),
-        '/homepage':(context) => const Homepage(),
-        '/navbar':(context) => const Navbar(),
-        '/loanapplication':(context) => const LoanApplication(),
-        '/loanamount':(context) => const LoanAmount(),
-        '/loandeliverymethod':(context) => const DeliveryMethod(),
-        '/selectbank':(context) =>  const SelectBank(),
-        '/selectmomo':(context) =>  const SelectMomo(),
-        '/selectcard':(context) => const SelectCard(),
-        '/bankreceipt':(context) => const BanKReceipt(),
-        '/cardreceipt':(context) => const CardReceipt(),
-        '/momoreceipt':(context) => const MomoReceipt(),
-        '/repayment':(context) => const Repayment(),
-        '/savingsmethod':(context) => const SavingsMethod(),
-        '/roundup':(context) => const RoundUp(),
-        '/navbar3':(context) => const Navbar3(),
-        
-      },
-  ),),);
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+        routes: {
+          '/': ((context) => const SplashScreen()),
+          '/onboarding':(context) => const Landing(),
+          '/phonefield':(context) => const PhoneField(),
+          '/otp':(context) => const Otp(),
+          '/basicinformation':(context) => const BasicInformation(),
+          '/login':(context) => const Login(),
+          '/idproof':(context) => const Proof(),
+          '/verifyfront':(context) => const Frontcard2(),
+          '/selfie':(context) => const Selfie(),
+          '/selfieverify':(context) => const Selfieverify(),
+          '/forgotpassword1':(context) => const Forgotpassword1(),
+          '/forgotpassword2':(context) => const Forgotpassword2(),
+          '/forgotpassword3':(context) => const Forgotpassword3(),
+          '/bankinformation':(context) => const  BankInformation(),
+          '/homepage':(context) => const Homepage(),
+          '/navbar':(context) => const Navbar(),
+          '/loanapplication':(context) => const LoanApplication(),
+          '/loanamount':(context) => const LoanAmount(),
+          '/loandeliverymethod':(context) => const DeliveryMethod(),
+          '/selectbank':(context) =>  const SelectBank(),
+          '/selectmomo':(context) =>  const SelectMomo(),
+          '/selectcard':(context) => const SelectCard(),
+          '/bankreceipt':(context) => const BanKReceipt(),
+          '/cardreceipt':(context) => const CardReceipt(),
+          '/momoreceipt':(context) => const MomoReceipt(),
+          '/repayment':(context) => const Repayment(),
+          '/savingsmethod':(context) => const SavingsMethod(),
+          '/roundup':(context) => const RoundUp(),
+          '/navbar3':(context) => const Navbar3(),
+          
+        },
+     ),
+   ),),);
 }
